23b21845b5068709ee20439db35d51da12c00a2d
Raza <6125955@stud.nau.edu.ua>
2023-04-11 13:39:00 +0300

    + deployment

--- mini-battle-backend/src/main.ts
+++ mini-battle-backend/src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   });
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
